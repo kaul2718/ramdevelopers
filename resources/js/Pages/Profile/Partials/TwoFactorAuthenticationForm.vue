e07fdfe2dc7d315e402daef59dbcad576586e38a
Ronaldo <bjorge171@gmail.com>
2025-12-18 16:51:13 -0500

    Ajustes en botones de envio

--- resources/js/Pages/Profile/Partials/TwoFactorAuthenticationForm.vue
+++ resources/js/Pages/Profile/Partials/TwoFactorAuthenticationForm.vue
@@ -191,7 +191,10 @@ const disableTwoFactorAuthentication = () => {
                 <div v-if="! twoFactorEnabled">
                     <ConfirmsPassword @confirmed="enableTwoFactorAuthentication">
                         <PrimaryButton type="button" :class="{ 'opacity-25': enabling }" :disabled="enabling">
-                            Habilitar
+                            <template #texto--boton>Habilitar</template>
+                            <template #icono--boton>
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                            </template>                            
                         </PrimaryButton>
                     </ConfirmsPassword>
                 </div>
@@ -205,7 +208,10 @@ const disableTwoFactorAuthentication = () => {
                             :class="{ 'opacity-25': enabling || confirmationForm.processing }"
                             :disabled="enabling || confirmationForm.processing"
                         >
-                            Confirmar
+                            <template #texto--boton>Confirmar</template>
+                            <template #icono--boton>
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                            </template>
                         </PrimaryButton>
                     </ConfirmsPassword>
 
--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -47,7 +47,22 @@ const updatePassword = () => {
             Asegúrate de que tu cuenta utiliza una contraseña larga y aleatoria para mantenerla segura.
         </template>
 
-        <template #form>
+        <template #form>            
+            <div class="contenedor--input">
+                <TextInput
+                    id="current_password"
+                    ref="currentPasswordInput"
+                    v-model="form.current_password"
+                    type="password"
+                    class="mt-1 block w-full"
+                    autocomplete="current-password"
+                />
+                <InputLabel for="current_password" value="Contraseña actual" />
+                <InputError :message="form.errors.current_password" class="mt-2" />
+            </div>
+
+            <div class="contenedor--imputs"></div>
+
             <div class="contenedor--input">
                 <TextInput
                     id="password"
@@ -72,19 +87,6 @@ const updatePassword = () => {
                 <InputLabel for="password_confirmation" value="Confirmar nueva contraseña" />
                 <InputError :message="form.errors.password_confirmation" class="mt-2" />
             </div>
-            
-            <div class="contenedor--input">
-                <TextInput
-                    id="current_password"
-                    ref="currentPasswordInput"
-                    v-model="form.current_password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    autocomplete="current-password"
-                />
-                <InputLabel for="current_password" value="Contraseña anterior" />
-                <InputError :message="form.errors.current_password" class="mt-2" />
-            </div>
         </template>
 
         <template #actions>
@@ -93,7 +95,10 @@ const updatePassword = () => {
             </ActionMessage>
 
             <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Guardar
+                <template #texto--boton>Guardar</template>
+                <template #icono--boton>
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                </template>
             </PrimaryButton>
         </template>
     </FormSection>
